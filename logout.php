516a984fe77cb0298b2fdba6e448d7370c5a1f0d
aiti-codes <153920227+aiti-codes@users.noreply.github.com>
2023-12-15 20:09:54 +0700

    file untuk login
    login php ke mysql

---
+++ logout.php
@@ -0,0 +1,15 @@
+<!-- logout.php -->
+<?php
+// Mulai sesi
+session_start();
+
+// Hapus semua data sesi
+session_unset();
+
+// Hancurkan sesi
+session_destroy();
+
+// Alihkan pengguna ke halaman login
+header("Location: index.php");
+exit();
+?>
---
+++ sukses_login.php
@@ -0,0 +1,19 @@
+<!-- halaman_sukses.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Sukses</title>
+</head>
+<body>
+    <h2>Selamat datang di Halaman Sukses!</h2>
+    
+    <!-- Tambahkan menu logout -->
+    <p><a href="logout.php">Logout</a></p>
+
+    <!-- Konten halaman sukses lainnya -->
+    <!-- ... -->
+
+</body>
+</html>
